d1459aee2167be6b506308fb97c8eac8ef1a8907
SamuelOmodele <samuelomodele19@gmail.com>
2024-05-20 15:27:24 +0100

    update

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './navigation.css'
 import { Link } from 'react-router-dom'
 
@@ -6,20 +6,33 @@ type NavigationProps = {
   current_link: string;
 }
 
+
+
 const Navigation  = ({ current_link } : NavigationProps) => {
+
+
+  useEffect(() => {
+
+    setShowNav(false);
+    console.log('set to false')
+
+  }, [current_link])
+
+  const [showNav, setShowNav] = useState<boolean>(false);
   return (
     <nav>
       <div className="logo">
         <div className='icon'>M</div>
         <span>Medicare Clinic</span>
       </div>
-      <ul>
+      <ul className= {showNav ? 'show' : ''}>
         <li className={current_link === 'home' ? 'active' : ''}><Link to='/'>Home</Link></li>
         <li className={current_link === 'services' ? 'active' : ''}><Link to=''>Services</Link></li>
         <li className={current_link === 'apps' ? 'active' : ''}><Link to=''>Apps</Link></li>
         <li className={current_link === 'about us' ? 'active' : ''}><Link to=''>About us</Link></li>
         <li className={current_link === 'login' ? 'active' : ''}><Link to='/login'>Login</Link></li>
       </ul>
+      <div className='menu-container'><span className="material-symbols-outlined menu-icon" onClick={() => setShowNav(prev => !prev)}>menu</span></div>
     </nav>
   )
 }
